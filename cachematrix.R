e64bf3daa164dcbe141f2a8139af3b70f42cd57f
Jessica Grindheim <grindhej@gene.com>
2024-08-01 19:32:27 +0000

    R_programming project 2.  let's hope this commit works when I'm in a session for another project

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## coursera R_programming project 2
+## There's an example of caching the mean of a vector,
+## which is confusing because there's no working example to see how it works
+## thumbs down to that
+## in this assignment, you do the same thing, but make the pair of functions
+## cache the inverse of a matrix (whatever that is)
+## instead of computing the mean of a vector
 
-## Write a short comment describing this function
+# function that can cache the inverse
+# This function makes a vector, which is really a list of 4 functions
+makeCacheMatrix <- function( m = matrix() ) {
+  # init
+  toReturn = list()
+  i <- NULL # inverse
+  
+  # FUN1:  set the matrix
+  set <- function( matrix ) { 
+    m <<- matrix
+    i <<- NULL
+  }
+  # FUN2:  get the value of a vector
+  get <- function(){ m } 
+  # FUN3: set the value of the inverse of the matrix
+  set.inverse <- function( inverse ){ i <<- inverse } 
+  # FUN4: get value of the mean
+  get.inverse <- function(){ i } 
+  
+  # finalize
+  toReturn[[ "set" ]] = set
+  toReturn[[ "get" ]] = get
+  toReturn[[ "set.inverse" ]] = set.inverse
+  toReturn[[ "get.inverse" ]] = get.inverse
+  
+  return(toReturn) }
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = makeCacheMatrix , ...) {
+  
+  m <- x$get.inverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  # calculate the inverse using matrix multiplication
+  
+  m <- solve(data)  %*% data
+  x$set.inverse(m)
+  
+  return(m)
 }
